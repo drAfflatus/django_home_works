17eceb73bdb20cad787fa954363e0419238a4d0f
Alexander Suvorov <drafflatus@rambler.ru>
2025-01-22 22:06:40 +0300

    changed files

--- 1.1-first-project/first_project/app/views.py
+++ 1.1-first-project/first_project/app/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render, reverse
-
+import datetime
+from os import listdir
 
 def home_view(request):
     template_name = 'app/home.html'
@@ -23,7 +24,7 @@ def home_view(request):
 def time_view(request):
     # обратите внимание – здесь HTML шаблона нет, 
     # возвращается просто текст
-    current_time = None
+    current_time = datetime.datetime.now()
     msg = f'Текущее время: {current_time}'
     return HttpResponse(msg)
 
@@ -32,4 +33,7 @@ def workdir_view(request):
     # по аналогии с `time_view`, напишите код,
     # который возвращает список файлов в рабочей 
     # директории
-    raise NotImplemented
+    list_dir=listdir(path='.')
+    msg = ' '.join(list_dir)
+    return HttpResponse(msg)
+    # raise NotImplemented
