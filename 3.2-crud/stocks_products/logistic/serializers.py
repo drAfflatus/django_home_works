63100b24bff2927a134349201c34946f7bd07e4a
Alexander Suvorov <drafflatus@rambler.ru>
2025-02-25 20:54:06 +0300

    changed files for task about CRUD logic

--- 3.2-crud/stocks_products/logistic/serializers.py
+++ 3.2-crud/stocks_products/logistic/serializers.py
@@ -1,20 +1,29 @@
 from rest_framework import serializers
-
+from .models import Product,Stock,StockProduct
 
 class ProductSerializer(serializers.ModelSerializer):
     # настройте сериализатор для продукта
-    pass
+    class Meta:
+        model = Product
+        fields = ['id','title','description']
+
 
 
 class ProductPositionSerializer(serializers.ModelSerializer):
     # настройте сериализатор для позиции продукта на складе
-    pass
+    product = ProductSerializer
+    class Meta:
+        model = StockProduct
+        fields = ['product','quantity','price']
 
 
 class StockSerializer(serializers.ModelSerializer):
     positions = ProductPositionSerializer(many=True)
 
     # настройте сериализатор для склада
+    class Meta:
+        model = Stock
+        fields = ['id','address','positions']
 
     def create(self, validated_data):
         # достаем связанные данные для других таблиц
@@ -26,7 +35,8 @@ class StockSerializer(serializers.ModelSerializer):
         # здесь вам надо заполнить связанные таблицы
         # в нашем случае: таблицу StockProduct
         # с помощью списка positions
-
+        for pos in positions:
+            StockProduct.objects.create(stock_id=stock.id, **pos)
         return stock
 
     def update(self, instance, validated_data):
@@ -39,5 +49,14 @@ class StockSerializer(serializers.ModelSerializer):
         # здесь вам надо обновить связанные таблицы
         # в нашем случае: таблицу StockProduct
         # с помощью списка positions
+        for pos in positions:
+            obj, created = StockProduct.objects.filter(stock_id=stock.id).update_or_create(
+                stock=stock,
+                product=pos['product'],
+                defaults={
+                    'quantity': pos['quantity'],
+                    'price': pos['price']
+                }
+            )
 
         return stock
